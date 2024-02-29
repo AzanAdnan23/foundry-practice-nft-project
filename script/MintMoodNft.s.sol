9c71e540afce7053b1d31350bf9c56e90799370a
Azan Adnan <azanbutt2311@gmail.com>
2024-02-29 19:09:29 +0500

    Add DevOpsTools import and implement mintNft function

--- script/MintMoodNft.s.sol
+++ script/MintMoodNft.s.sol
@@ -4,3 +4,25 @@ pragma solidity ^0.8.20;
 
 import {Script, console} from "forge-std/Script.sol";
 import {MoodNft} from "../src/MoodNft.sol";
+import {DevOpsTools} from "lib/foundry-devops/src/DevOpsTools.sol";
+
+contract MintMoodNft is Script {
+    function run() external {
+        vm.startBroadcast();
+        address mostRecentDeployed = DevOpsTools.get_most_recent_deployment(
+            "MoodNft",
+            block.chainid
+        );
+        vm.stopBroadcast();
+
+        mintNft(mostRecentDeployed);
+    }
+
+    function mintNft(address contractAddress) internal {
+        vm.startBroadcast();
+
+        MoodNft(contractAddress).mintNft();
+
+        vm.stopBroadcast();
+    }
+}
